b2ed6526b6cfbb3c4d266b8bb041e2482b8aa5be
ZManzanares87 <zoraidamanzanaresj@gmail.com>
2023-07-20 17:28:59 +0200

    subiendo imagene

--- frontend/src/components/Content/Content.jsx
+++ frontend/src/components/Content/Content.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import './Content.css'; 
 
+
+
 const Content = () => {
   const [content, setContent] = useState([]);
 
@@ -21,16 +23,19 @@ const Content = () => {
   return (
     <div>
       <h1>Productos</h1>
-      <div id='productos-container'>
-        {
-          content.map((item) => ( 
-            <div key={item._id} className='data'>
-              <h4>{item.titulo}</h4>
-              <p>Descripcion:{item.descripcion}</p>
-              <p>Precio:{item.precio}</p>
-              <img src={item.image_url} alt="Descripcion de cada viaje" />
+      <div id="productos-container">
+        {content.map((item) => (
+          <div key={item._id} className="data">
+            <img src={item.image_url} alt="Descripción de cada viaje" />
+            <div className="card-body">
+              <h5 className="card-title">{item.titulo}</h5>
+              <p className="card-text">Descripcion: {item.descripcion}</p>
+              <p className="card-text">Precio: {item.precio}</p>
+              {/* Si tienes un enlace para ir a algún lugar, puedes usar el siguiente botón */}
+              {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
             </div>
-          ))}
+          </div>
+        ))}
       </div>
     </div>
   );
