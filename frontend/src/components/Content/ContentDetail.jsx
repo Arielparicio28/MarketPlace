0ed7238fcb5aa21ce42930943a6a640a9012eb7c
Arielparicio28 <arielaparicio100@gmail.com>
2023-07-20 18:12:51 +0200

    Filtrando contenido

--- frontend/src/components/Content/ContentDetail.jsx
+++ frontend/src/components/Content/ContentDetail.jsx
@@ -1,16 +1,47 @@
 import React,{ useEffect, useState } from 'react'
 import { useParams } from "react-router-dom";
-
+import axios from "axios";
 
 
 const ContentDetail =() => {
 const[content,setContent]= useState(null);
 const {contentId} = useParams();
-console.log(contentId)
+
+useEffect(()=> {
+  const fetchContent = async() => {
+    try{
+      const response = await axios.get(`http://localhost:3000/content/${contentId}`)
+      const data = response.data
+      setContent(data)
+    }catch(error){
+      console.log(error)
+    }
+  };
+  fetchContent()
+},[contentId])
+
+
+
 
 
   return (
-    <div>ContentDetail</div>
+<div>
+{content && (
+<>
+<img src={content.image_url}  width={100}/>
+<h4>Descripcion:{content.descripcion}</h4>
+
+
+</>
+
+
+)}
+
+</div>
+
+
+
+
   )
 }
 
