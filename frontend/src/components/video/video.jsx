36c313940ef84593aaab39788cedfd345349afc3
Arielparicio28 <arielaparicio100@gmail.com>
2023-07-20 23:52:46 +0200

    Casi terminado

--- frontend/src/components/video/video.jsx
+++ frontend/src/components/video/video.jsx
@@ -14,9 +14,8 @@ const Video = () => {
       <source src='https://cdn.glitch.me/ff80f9d6-a64f-42f1-9850-de7d0b3fdd5d/pexels_videos_2421545%20(2160p).mp4?v=1689849713814' type='video/mp4'></source>
     </video>
     <div className='content'>
-      <h1>¡Vive la Historia,Hoy</h1>
-      <p>Descubre momentos históricos re-creados con inteligencia artificial y
-        sumérgete en la experiencia como si estuvieras allí.</p>
+      <h1>DREAMS</h1>
+      <p>Descubre todo el poder de la IA y la realidad virtual</p>
       
     </div>
   </div>
